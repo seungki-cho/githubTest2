1231330b036f576d2a45b291c0852d5380e30217
seungki-cho <beenzino@kookmin.ac.kr>
2020-10-24 00:54:02 +0900

    git init

---
+++ githubTest2/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  githubTest2
+//
+//  Created by 조승기 on 2020/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int cnt_i;
+    
+    printf("Hello world!\n");
+    printf("%s\n",argv[0]);
+    for (cnt_i = 0; cnt_i<argc; cnt_i++) {
+        printf("%s\n",argc[cnt_i]);
+    }
+    return 0;
+}
